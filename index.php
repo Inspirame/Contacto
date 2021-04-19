5b47c74747b296fe6dafd2e4ec05cb47772d5a02
Inspire ME <82292429+Inspirame@users.noreply.github.com>
2021-04-18 20:39:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta charset="UTF-8">
+  <title>Inspire ME</title>
+  <link rel="stylesheet" href="index.css">
+  <link rel="icon" stylesheet="image/jpg" href="icon.png">
+</head>
+<body>
+  <div id="bmenu" align="center"><br><br><br><br><br>
+    <img src="bannerimg.png.png" width="360px" alt=""><br><br><br><br><br><br>
+<div id="menu"><br>
+    <a href="https://inspirame.github.io/inicio/" id="menu">Inicio</a>
+    <a href="https://inspirame.github.io/tienda/" id="menu">Tienda</a>
+    <a href="https://inspirame.github.io/contacto/" id="menu">Contacto</a></b>
+</div></div><br><br><br><br><br><center>
+  <div id="buttonllamada"><br>
+    <a href="tel:(811)0505904" id="llamar">+52 81 1050 5904</a><br><br>
+</div></center>
+</body>
+</html>
